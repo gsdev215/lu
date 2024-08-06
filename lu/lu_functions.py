322e44e2194e86b225604517c9782e4e8aa998f9
gsdev215 <dev1050d1@gmail.com>
2024-08-06 16:09:02 +0530

    added prototype funtion handerler -incomplete

---
+++ lu/lu_functions.py
@@ -0,0 +1,29 @@
+from typing import List, Dict, Any
+from lu_errors import LuError, LuSyntaxError, LuTypeError, LuNameError
+
+class LuFunction:
+    def __init__(self, name: str, parameters: List[Dict[str, str]], return_type: str, body: List[str]):
+        self.name = name
+        self.parameters = parameters
+        self.return_type = return_type
+        self.body = body
+
+class FunctionManager:
+    def __init__(self):
+        self.functions: Dict[str, LuFunction] = {}
+
+    def define_function(self, function: LuFunction):
+        if function.name in self.functions:
+            raise LuNameError(f"Function '{function.name}' is already defined", None)
+        self.functions[function.name] = function
+
+    def get_function(self, name: str) -> LuFunction:
+        if name not in self.functions:
+            raise LuNameError(f"Function '{name}' is not defined", None)
+        return self.functions[name]
+
+    def compile_function(self, function: LuFunction) -> List[str]:
+        py_tokens = [f"def {function.name}({', '.join(param['name'] for param in function.parameters)}):\n"]
+        for line in function.body:
+            py_tokens.append(f"    {line}\n")
+        return py_tokens
