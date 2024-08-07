8d703dd62472f7a5f700907d8cf7cba31fc699b6
gsdev215 <dev1050d1@gmail.com>
2024-08-07 15:48:43 +0530

    run test

--- lu/lu_compiler.py
+++ lu/lu_compiler.py
@@ -2,12 +2,17 @@ from lu_lexer import tokenize_text
 from lu_parser import Lu
 from lu_errors import Error
 from lu_logger import info, error, exception
+import argparse 
 
 def process_file(input_filename: str, output_filename: str):
     try:
         with open(input_filename, 'r', encoding='utf-8') as infile:
             text = infile.read()
-        
+        if text.startswith("run"):
+            run = True
+            text = text.removeprefix("run")
+        else:
+            run = False
         info(f"Processing file: {input_filename}")
         tokens = tokenize_text(text)
         lu_instance = Lu(tokens)
@@ -15,6 +20,12 @@ def process_file(input_filename: str, output_filename: str):
         
         with open(output_filename, 'w', encoding='utf-8') as outfile:
             outfile.writelines(py_tokens)
+        
+        if run:
+            with open(output_filename, 'r', encoding='utf-8') as outfile:
+                code = outfile.read()
+                exec(code)
+                exit()
         
         info(f"Compilation successful. Output written to {output_filename}")
     except Error as e:
@@ -24,7 +35,16 @@ def process_file(input_filename: str, output_filename: str):
     except Exception as e:
         exception(f"Unexpected error occurred: {str(e)}")
 
+
+def main():
+    parser = argparse.ArgumentParser(description='lu compiler')
+    parser.add_argument('input_file', type=str, help='Lu file path (e.g., path/example.lu )')
+    parser.add_argument('output_file', type=str, help='output python file path (e.g., path/output.py )')
+    args = parser.parse_args()
+    process_file(args.input_file, args.output_file)
+
 if __name__ == "__main__":
+    #main()
     input_file = 'lu/lu/example.lumin'
     output_file = 'output.py'
     process_file(input_file, output_file)
